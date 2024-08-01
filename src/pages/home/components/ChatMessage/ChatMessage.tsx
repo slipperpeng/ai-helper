32c7cdf0e879b9814bd0a5459e8a472ebed81fa3
slipper <1160823499@qq.com>
2024-08-02 00:10:11 +0800

    style:聊天框ui

--- src/pages/home/components/ChatMessage/ChatMessage.tsx
+++ src/pages/home/components/ChatMessage/ChatMessage.tsx
@@ -1,7 +1,17 @@
 import './ChatMessage.less'
-import Markdown from 'react-markdown'
+import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import {dark} from 'react-syntax-highlighter/dist/esm/styles/prism'
+import {
+  PlusCircleOutlined
+} from '@ant-design/icons'
+import { Button,Tooltip,Input  } from 'antd'
+import { useState } from 'react'
+
+const { TextArea } = Input;
 
 const ChatMessage = ()=>{
+  const [msgText,sendMegText] = useState('')
   return (
     <div className='chat-message-wrapper'>
       <div className="chat-message-scroll-wrapper">
@@ -15,7 +25,7 @@ const ChatMessage = ()=>{
                     <div className="message-right-side">
                       <div className="markdown-container">
                         <Markdown className="markdown">
-                          '删除\n删除'
+                          {'删除\n删除'}
                         </Markdown>
                       </div>
                     </div>
@@ -26,9 +36,51 @@ const ChatMessage = ()=>{
           </div>
         </div>
       </div>
-      <div className="chat-page-footer"></div>
+      <footer className="chat-page-footer">
+        <div className="func-area">
+          <Tooltip placement="top" title="选择附件">
+            <Button className='func-btn' type="text">
+              <PlusCircleOutlined />
+            </Button>
+          </Tooltip>
+        </div>
+        <TextArea
+          className="text-area"
+          placeholder="输入消息"
+          value={msgText}
+          variant="borderless"
+          onChange={(e) => sendMegText(e.target.value)}
+        >
+
+        </TextArea>
+      </footer>
     </div>
   )
 }
+
+const CodeBlock = (props) => {
+  const {children, className, ...rest} = props
+  const match = /language-(\w+)/.exec(className || '')
+  return match ? (
+    <SyntaxHighlighter
+      {...rest}
+      PreTag="div"
+      children={String(children).replace(/\n$/, '')}
+      language={match[1]}
+      style={dark}
+    />
+  ) : (
+    <code {...rest} className={className}>
+      {children}
+    </code>
+  )
+}
+
+const Markdown = ({ children, ...props }) => (
+  <ReactMarkdown {...props} components={{ code: CodeBlock }}>
+    {typeof children === 'string' ? children.replace(/\n/g, '  \n') : children}
+  </ReactMarkdown>
+  
+); 
 
 export default ChatMessage
